632d321836a57633697df8ca8283af1ae883f2ec
devinanderson12 <devinanderson142@gmail.com>
2016-09-26 10:37:50 -0500

    complete first question
    finished product

---
+++ WindowsFormsApplication4/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.happy = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.lblClick = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 79);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "MAD";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // happy
+            // 
+            this.happy.Location = new System.Drawing.Point(95, 78);
+            this.happy.Name = "happy";
+            this.happy.Size = new System.Drawing.Size(75, 23);
+            this.happy.TabIndex = 1;
+            this.happy.Text = "happy";
+            this.happy.UseVisualStyleBackColor = true;
+            this.happy.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(177, 78);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "SAD";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // lblClick
+            // 
+            this.lblClick.AutoSize = true;
+            this.lblClick.Location = new System.Drawing.Point(119, 62);
+            this.lblClick.Name = "lblClick";
+            this.lblClick.Size = new System.Drawing.Size(35, 13);
+            this.lblClick.TabIndex = 3;
+            this.lblClick.Text = "label1";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(95, 107);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "OTHER";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.lblClick);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.happy);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button happy;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label lblClick;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
